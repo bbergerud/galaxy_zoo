6dd1d6b34f075f36cd74e66fa90c73e8137d5f99
Bergerud <bergerud@r-lnx701.physics.uiowa.edu>
2017-04-10 12:27:23 -0500

    python3 download updated

--- data/GZ1/download3.py
+++ data/GZ1/download3.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
+import sys, os
 import urllib.parse
 import urllib.request
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-
-def download(type, space=100):
+def download(type, nPetro=1.75, impix=299, nprint=500):
 	"""
 	Function for downloading the GZ1 dataset associated
 	with the "type" csv file.
 
 	To call:
-		download(type)
+		download(type, nPetro, impix)
 
 	Parameters:
-		type		"E", "Edge-on", or "S"
+		type		"E", "S0", or "S"
+		nPetro		number of Pertrosian radii
+		impix		image size (impix X impix)
 
 	Postcondtion:
 		The files are stored in the folder specified
@@ -25,26 +27,42 @@ def download(type, space=100):
 	# ==================================================
 	#	Open the csv file
 	# ==================================================
-	df = pd.read_csv(type + ".csv")
+	df = pd.read_csv('csv/' + type + ".csv")
 
 	# ==================================================
-	#	Convert the coordinates to decimal
+	#	Test to see if the directories for placing
+	#	the images exist. If not, create directory
 	# ==================================================
-	coord = [a + " " + b for a, b in zip(df["RA"], df["DEC"])]
-	coord = SkyCoord(coord, unit=(u.hour, u.deg), frame='icrs')
+	groups = set(df['group'])
+	for group in groups:
+		dir = 'data/' + group + '/' + type
+		if not os.path.exists(dir):
+			os.makedirs(dir)
 
 	# ==================================================
 	#	Set the image properties and retrieve the url
+	#	Set the total number of arcseconds equal to
+	#	2x the desired petrosian radius (diameter)
 	# ==================================================
-	impix = 207
-	scale = 0.5
+	narc = 2. * nPetro * df["petroRad_g"].values
+	scales = narc / impix
+
 	cutoutbaseurl = 'http://skyservice.pha.jhu.edu/DR12/ImgCutout/getjpeg.aspx'
 
-	N = df["OBJID"].size
-	for i, (ra, dec, objid) in enumerate(zip(coord.ra.deg, coord.dec.deg, df["OBJID"].astype(str))):
-		if i % space == 0:
-			print("Iter #{:d} / {:d}".format(i, N))
 
+	N = df.shape[0]
+	for i, (ra, dec, objid, group, scale) in enumerate(zip(df['ra'], df['dec'], df["objid"].astype(str), df["group"], scales)):
+
+		# ==================================================
+		#	Print the progress
+		# ==================================================
+		if i % nprint == 0:
+			print("Iter #{:d} / {:d}".format(i, N))
+		
+
+		# ==================================================
+		#	Create the query string
+		# ==================================================
 		query_string = urllib.parse.urlencode(dict(ra=ra, 
 							dec=dec, 
 							width=impix, 
@@ -55,5 +73,5 @@ def download(type, space=100):
 
 		# ==================================================
 		#	Download the image
-		# ==================================================	
-		urllib.request.urlretrieve(url, type + "/" + objid + ".jpg")
+		# ==================================================
+		urllib.request.urlretrieve(url, "data/" + group + "/" + type + "/" + objid + ".jpg")
